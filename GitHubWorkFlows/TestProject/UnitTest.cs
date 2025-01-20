63e3e8f15fea56de42c57f0122ac13aea9e7e72d
kareem.mohamed <kareem.mohamed@linkdev.com>
2025-01-20 08:21:25 +0200

    Project Creation

---
+++ GitHubWorkFlows/TestProject/UnitTest.cs
@@ -0,0 +1,29 @@
+namespace TestProject
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void AdditionTest()
+        {
+            Assert.Equal(2, 1 + 1);
+        }
+
+        [Fact]
+        public void SubtractionTest()
+        {
+            Assert.Equal(0, 1 - 1);
+        }
+
+        [Fact]
+        public void MultiplicationTest()
+        {
+            Assert.Equal(1, 1 * 1);
+        }
+
+        [Fact]
+        public void DivisionTest()
+        {
+            Assert.Equal(1, 1 / 1);
+        }
+    }
+}
